3af52883321b901dc36d0ec25aa282a3ae5aeee9
Sandeep Vidyapu <sandy.apj911@gmail.com>
2024-12-30 20:21:35 +0100

    add joins

--- basics.sql
+++ basics.sql
@@ -37,9 +37,57 @@ SELECT MAX(shippeddate - orderdate) FROM orders WHERE shipcountry = 'France'; --
 SELECT AVG(quantity) FROM order_details WHERE productid=35; -- AVG
 SELECT companyname FROM customers WHERE companyname LIKE '_%er' -- %: match 0 or more chars; _: match one char
 -- FROM part and WHERE part are evaluated before the SELECT part.
--- Therefore, Alias (defined using AS keyword) can only be used in ORDER BY and GROUP BY (but not in WHERE, FROM, HAVING)
+-- Therefore, Column Aliases (defined using AS keyword) can only be used in ORDER BY and GROUP BY (but not in WHERE, FROM, HAVING)
 SELECT unitsinstock*unitprice AS totalinventory FROM products ORDER BY totalinventory DESC;
 SELECT productname, unitsinstock*unitprice AS totalinventory FROM products ORDER BY totalinventory, productname ASC LIMIT 2; -- LIMIT
 -- NULL does not mean, the value is empty, but UNKOWN.
+SELECT COUNT(*) FROM orders WHERE shipregion IS NULL;
+
+
+-- JOINS
+-- INNER JOIN
+SELECT companyname, productname, categoryname, orderdate, order_details.unitprice, quantity 
+FROM orders 
+JOIN order_details ON orders.orderid=order_details.orderid
+JOIN customers ON customers.customerid=orders.customerid
+JOIN products ON products.productid=order_details.productid
+JOIN categories ON categories.categoryid=products.categoryid
+WHERE categoryname = 'Seafood' AND 
+	order_details.unitprice*quantity >= 500;
+
+-- LEFT JOIN
+SELECT productname, orderid 
+FROM products
+LEFT JOIN order_details ON products.productid=order_details.productid
+WHERE orderid IS NULL;
+
+-- RIGHT JOIN
+SELECT companyname, customercustomerdemo.customerid
+FROM customercustomerdemo
+RIGHT JOIN customers ON customercustomerdemo.customerid=customers.customerid;
+
+-- FULL JOIN
+SELECT categoryname, productname
+FROM categories
+FULL JOIN products ON categories.categoryid=products.categoryid;
+
+-- SELF JOIN (NOTE: there is no keyword as SELF JOIN)
+SELECT s1.companyname AS SupplierCompany1, s2.companyname AS SupplierCompany2, s1.country
+FROM suppliers s1
+JOIN suppliers s2 ON s1.country=s2.country
+WHERE s1.supplierid <> s2.supplierid
+ORDER BY s1.country;
+
+-- USING
+SELECT * 
+FROM orders
+JOIN order_details USING (orderid)
+JOIN products USING (productid);
+
+-- NATURAL JOIN (check on all matching column names)
+SELECT * 
+FROM order_details
+NATURAL JOIN orders
+NATURAL JOIN customers;
 
 
