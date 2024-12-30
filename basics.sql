8fafe3c27a50ca443665cbd3dbf6c380e8671461
Sandeep Vidyapu <sandy.apj911@gmail.com>
2024-12-30 14:46:53 +0100

    order by, avg, min, max, limit, psql

--- basics.sql
+++ basics.sql
@@ -21,3 +21,25 @@ SELECT COUNT(*) FROM products WHERE categoryid IN (1,4,6,7); -- IN
 -- Schema
 SELECT * FROM purchasing.vendor; -- purchasing schema
 
+-- psql
+psql --port=5432 --host=localhost --dbname=pagila --username=postgres -- execute this on commandline
+\l -- list of databases
+\! clear -- clear screen
+\c northwind -- connect to database northwind
+\dn -- list of schemas
+\dt sales. -- list of tables in schema sales (do not forget the dot at the end)
+-- semicolon is mandatory for statements executed inside psql
+
+
+-- Intermediate Select Statements
+SELECT productname, unitprice FROM products ORDER BY unitprice DESC, productname ASC; -- OREDER BY
+SELECT MAX(shippeddate - orderdate) FROM orders WHERE shipcountry = 'France'; -- MIN and MAX
+SELECT AVG(quantity) FROM order_details WHERE productid=35; -- AVG
+SELECT companyname FROM customers WHERE companyname LIKE '_%er' -- %: match 0 or more chars; _: match one char
+-- FROM part and WHERE part are evaluated before the SELECT part.
+-- Therefore, Alias (defined using AS keyword) can only be used in ORDER BY and GROUP BY (but not in WHERE, FROM, HAVING)
+SELECT unitsinstock*unitprice AS totalinventory FROM products ORDER BY totalinventory DESC;
+SELECT productname, unitsinstock*unitprice AS totalinventory FROM products ORDER BY totalinventory, productname ASC LIMIT 2; -- LIMIT
+-- NULL does not mean, the value is empty, but UNKOWN.
+
+
